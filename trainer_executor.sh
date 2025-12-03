5e9d1c0a559829d2ef4d2fbfc91f0db5ee4f658e
xavifeme00 <xavifeme00@gmial.com>
2025-12-03 14:06:19 +0100

    Code

--- trainer_executor.sh
+++ trainer_executor.sh
@@ -1,5 +1,34 @@
 mkdir experiments
 
+# CM 4
+./bin/trainer imgs/int20040906_00421872_img0_u16be-1x4200x2154.raw 4 0.001 300 > experiments/INT_4_0-001.txt
+./bin/trainer imgs/int20040906_00421872_img0_u16be-1x4200x2154.raw 4 0.01 300 > experiments/INT_4_0-01.txt
+./bin/trainer imgs/int20040906_00421872_img0_u16be-1x4200x2154.raw 4 0.1 300 > experiments/INT_4_0-1.txt
+
+./bin/trainer imgs/0004692896-20240825-OSIRIS-OsirisBroadBandImage_u16be-1x2056x2073.raw 4 0.001 300 > experiments/GTC_4_0-001.txt
+./bin/trainer imgs/0004692896-20240825-OSIRIS-OsirisBroadBandImage_u16be-1x2056x2073.raw 4 0.01 300 > experiments/GTC_4_0-01.txt
+./bin/trainer imgs/0004692896-20240825-OSIRIS-OsirisBroadBandImage_u16be-1x2056x2073.raw 4 0.1 300 > experiments/GTC_4_0-1.txt
+
+./bin/trainer imgs/jkt19990925_00100583_img0-u16be-1x2148x2148.raw 4 0.001 300 > experiments/JKT_4_0-001.txt
+./bin/trainer imgs/jkt19990925_00100583_img0-u16be-1x2148x2148.raw 4 0.01 300 > experiments/JKT_4_0-01.txt
+./bin/trainer imgs/jkt19990925_00100583_img0-u16be-1x2148x2148.raw 4 0.1 300 > experiments/JKT_4_0-1.txt
+
+./bin/trainer imgs/wht20120123_01754958_img0-u16be-1x2501x2148.raw 4 0.001 300 > experiments/WHT_4_0-001.txt
+./bin/trainer imgs/wht20120123_01754958_img0-u16be-1x2501x2148.raw 4 0.01 300 > experiments/WHT_4_0-01.txt
+./bin/trainer imgs/wht20120123_01754958_img0-u16be-1x2501x2148.raw 4 0.1 300 > experiments/WHT_4_0-1.txt
+
+./bin/trainer imgs/coj0m403-kb24-20220213-0274-e00_img0-u16be-1x2112x3136.raw 4 0.001 300 > experiments/LCO_4_0-001.txt
+./bin/trainer imgs/coj0m403-kb24-20220213-0274-e00_img0-u16be-1x2112x3136.raw 4 0.01 300 > experiments/LCO_4_0-01.txt
+./bin/trainer imgs/coj0m403-kb24-20220213-0274-e00_img0-u16be-1x2112x3136.raw 4 0.1 300 > experiments/LCO_4_0-1.txt
+
+./bin/trainer imgs/ERIS.2024-01-01T06:15:12.592_u16be-1x2046x2048.raw 4 0.001 300 > experiments/VLT_4_0-001.txt
+./bin/trainer imgs/ERIS.2024-01-01T06:15:12.592_u16be-1x2046x2048.raw 4 0.01 300 > experiments/VLT_4_0-01.txt
+./bin/trainer imgs/ERIS.2024-01-01T06:15:12.592_u16be-1x2046x2048.raw 4 0.1 300 > experiments/VLT_4_0-1.txt
+
+./bin/trainer imgs/TJO2459273.28051_V_imr_img0-u16be-1x4108x4096.raw 4 0.001 300 > experiments/TJO_4_0-001.txt
+./bin/trainer imgs/TJO2459273.28051_V_imr_img0-u16be-1x4108x4096.raw 4 0.01 300 > experiments/TJO_4_0-01.txt
+./bin/trainer imgs/TJO2459273.28051_V_imr_img0-u16be-1x4108x4096.raw 4 0.1 300 > experiments/TJO_4_0-1.txt
+
 # CM 0
 ./bin/trainer imgs/int20040906_00421872_img0_u16be-1x4200x2154.raw 0 0.001 300 > experiments/INT_0_0-001.txt
 ./bin/trainer imgs/int20040906_00421872_img0_u16be-1x4200x2154.raw 0 0.01 300 > experiments/INT_0_0-01.txt
@@ -115,32 +144,3 @@ mkdir experiments
 ./bin/trainer imgs/TJO2459273.28051_V_imr_img0-u16be-1x4108x4096.raw 3 0.001 300 > experiments/TJO_3_0-001.txt
 ./bin/trainer imgs/TJO2459273.28051_V_imr_img0-u16be-1x4108x4096.raw 3 0.01 300 > experiments/TJO_3_0-01.txt
 ./bin/trainer imgs/TJO2459273.28051_V_imr_img0-u16be-1x4108x4096.raw 3 0.1 300 > experiments/TJO_3_0-1.txt
-
-# CM 4
-./bin/trainer imgs/int20040906_00421872_img0_u16be-1x4200x2154.raw 4 0.001 300 > experiments/INT_4_0-001.txt
-./bin/trainer imgs/int20040906_00421872_img0_u16be-1x4200x2154.raw 4 0.01 300 > experiments/INT_4_0-01.txt
-./bin/trainer imgs/int20040906_00421872_img0_u16be-1x4200x2154.raw 4 0.1 300 > experiments/INT_4_0-1.txt
-
-./bin/trainer imgs/0004692896-20240825-OSIRIS-OsirisBroadBandImage_u16be-1x2056x2073.raw 4 0.001 300 > experiments/GTC_4_0-001.txt
-./bin/trainer imgs/0004692896-20240825-OSIRIS-OsirisBroadBandImage_u16be-1x2056x2073.raw 4 0.01 300 > experiments/GTC_4_0-01.txt
-./bin/trainer imgs/0004692896-20240825-OSIRIS-OsirisBroadBandImage_u16be-1x2056x2073.raw 4 0.1 300 > experiments/GTC_4_0-1.txt
-
-./bin/trainer imgs/jkt19990925_00100583_img0-u16be-1x2148x2148.raw 4 0.001 300 > experiments/JKT_4_0-001.txt
-./bin/trainer imgs/jkt19990925_00100583_img0-u16be-1x2148x2148.raw 4 0.01 300 > experiments/JKT_4_0-01.txt
-./bin/trainer imgs/jkt19990925_00100583_img0-u16be-1x2148x2148.raw 4 0.1 300 > experiments/JKT_4_0-1.txt
-
-./bin/trainer imgs/wht20120123_01754958_img0-u16be-1x2501x2148.raw 4 0.001 300 > experiments/WHT_4_0-001.txt
-./bin/trainer imgs/wht20120123_01754958_img0-u16be-1x2501x2148.raw 4 0.01 300 > experiments/WHT_4_0-01.txt
-./bin/trainer imgs/wht20120123_01754958_img0-u16be-1x2501x2148.raw 4 0.1 300 > experiments/WHT_4_0-1.txt
-
-./bin/trainer imgs/coj0m403-kb24-20220213-0274-e00_img0-u16be-1x2112x3136.raw 4 0.001 300 > experiments/LCO_4_0-001.txt
-./bin/trainer imgs/coj0m403-kb24-20220213-0274-e00_img0-u16be-1x2112x3136.raw 4 0.01 300 > experiments/LCO_4_0-01.txt
-./bin/trainer imgs/coj0m403-kb24-20220213-0274-e00_img0-u16be-1x2112x3136.raw 4 0.1 300 > experiments/LCO_4_0-1.txt
-
-./bin/trainer imgs/ERIS.2024-01-01T06:15:12.592_u16be-1x2046x2048.raw 4 0.001 300 > experiments/VLT_4_0-001.txt
-./bin/trainer imgs/ERIS.2024-01-01T06:15:12.592_u16be-1x2046x2048.raw 4 0.01 300 > experiments/VLT_4_0-01.txt
-./bin/trainer imgs/ERIS.2024-01-01T06:15:12.592_u16be-1x2046x2048.raw 4 0.1 300 > experiments/VLT_4_0-1.txt
-
-./bin/trainer imgs/TJO2459273.28051_V_imr_img0-u16be-1x4108x4096.raw 4 0.001 300 > experiments/TJO_4_0-001.txt
-./bin/trainer imgs/TJO2459273.28051_V_imr_img0-u16be-1x4108x4096.raw 4 0.01 300 > experiments/TJO_4_0-01.txt
-./bin/trainer imgs/TJO2459273.28051_V_imr_img0-u16be-1x4108x4096.raw 4 0.1 300 > experiments/TJO_4_0-1.txt
