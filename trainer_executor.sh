5aa97abcc740c44c102e7f8537ca9efce8bd3655
xavifeme00 <xavifeme00@gmial.com>
2025-12-10 11:58:18 +0100

    Code

--- trainer_executor.sh
+++ trainer_executor.sh
@@ -2,37 +2,10 @@
 
 mkdir -p experiments
 
-imgs=(
-    "INT imgs/int20040906_00421872_img0_u16be-1x4200x2154.raw"
-    "GTC imgs/0004692896-20240825-OSIRIS-OsirisBroadBandImage_u16be-1x2056x2073.raw"
-    "JKT imgs/jkt19990925_00100583_img0-u16be-1x2148x2148.raw"
-    "WHT imgs/wht20120123_01754958_img0-u16be-1x2501x2148.raw"
-    "LCO imgs/coj0m403-kb24-20220213-0274-e00_img0-u16be-1x2112x3136.raw"
-    "VLT imgs/ERIS.2024-01-01T06:15:12.592_u16be-1x2046x2048.raw"
-    "TJO imgs/TJO2459273.28051_V_imr_img0-u16be-1x4108x4096.raw"
-)
-
-cms=(4 3 2 1 0)
-
-rls=(0.1 0.01 0.005 0.001)
-
-eps=(0.1 0.01 0.001 0.0001)
-
-epochs=300
-
-echo "== Llençant GRID SEARCH =="
-
-for cm in "${cms[@]}"; do
-    for entry in "${imgs[@]}"; do
-        for rl in "${rls[@]}"; do
-            for ep in "${eps[@]}"; do
-                read -r name path <<< "$entry"
-
-                out="experiments/${name}_${cm}_${rl}.txt"
-
-                echo "Executant: $name CM=$cm RL=$rl EP=$ep"
-                ./bin/trainer "$path" "$cm" "$rl" "$ep" "$epochs" > "$out"
-            done
-        done
-    done
-done
+./bin/trainer ./INT.txt 4 0.001 0.001 60 > experiments/INT_cm4.txt
+./bin/trainer ./GTC.txt 4 0.001 0.001 60 > experiments/GTC_cm4.txt
+./bin/trainer ./JKT.txt 4 0.001 0.001 60 > experiments/JKT_cm4.txt
+./bin/trainer ./WHT.txt 4 0.001 0.001 60 > experiments/WHT_cm4.txt
+./bin/trainer ./LCO.txt 4 0.001 0.001 60 > experiments/LCO_cm4.txt
+./bin/trainer ./VLT.txt 4 0.001 0.001 60 > experiments/VLT_cm4.txt
+./bin/trainer ./TJO.txt 4 0.001 0.001 60 > experiments/TJO_cm4.txt
